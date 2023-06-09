873187ff5c9aa428a30864780f89b2b9b173fbc9
E5c11 <emmanuelconradie@gmail.com>
2023-06-09 19:30:18 +0200

    app test dependencies create RemoteWeatherDataSourceTest

---
+++ app/src/test/java/com/demo/weather/weather/RemoteWeatherDataSourceTest.kt
@@ -0,0 +1,68 @@
+package com.demo.weather.weather
+
+import com.demo.weather.weather.data.City
+import com.demo.weather.weather.data.FiveDayWeather
+import com.demo.weather.weather.data.Weather
+import com.demo.weather.weather.io.WeatherApi
+import com.demo.weather.weather.io.WeatherFetchException
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.amshove.kluent.internal.assertFailsWith
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import retrofit2.Response
+import java.io.IOException
+
+@RunWith(JUnit4::class)
+@OptIn(ExperimentalCoroutinesApi::class)
+class RemoteWeatherDataSourceTest {
+
+    private val weatherApi: WeatherApi = mockk()
+
+    private val repo = RemoteWeatherDataSource(weatherApi)
+
+    private val mockData = FiveDayWeather(
+        "",
+        123,
+        321,
+        listOf<Weather>(),
+        City()
+    )
+
+    @Test
+    fun `A Successful getFiveDayWeather fetch returns FiveDayWeather object`() = runTest {
+        val response = Response.success(mockData)
+        coEvery { weatherApi.getFiveDay(any(), any(), any(), any(), any()) } returns response
+
+        repo.getFiveDayWeather(123L, 321L).shouldBeEqualTo(mockData)
+
+        coVerify(exactly = 1) { weatherApi.getFiveDay(any(), any(), any(), any(), any()) }
+    }
+
+    @Test
+    fun `A Failed getFiveDayWeather fetch throws WeatherFetchException`() = runTest {
+
+        coEvery { weatherApi.getFiveDay(any(), any(), any(), any(), any()) } throws IOException()
+
+        assertFailsWith<WeatherFetchException> { repo.getFiveDayWeather(123L, 321L) }
+
+        coVerify(exactly = 1) { weatherApi.getFiveDay(any(), any(), any(), any(), any()) }
+    }
+
+    @Test
+    fun `A Failed getFiveDayWeather fetch returns a Exception from the API`() = runTest {
+        val response = Response.error<FiveDayWeather>(401, "".toResponseBody(null))
+        coEvery { weatherApi.getFiveDay(any(), any(), any(), any(), any()) } returns response
+
+        assertFailsWith<WeatherFetchException> { repo.getFiveDayWeather(123L, 321L) }
+
+        coVerify(exactly = 1) { weatherApi.getFiveDay(any(), any(), any(), any(), any()) }
+    }
+}
