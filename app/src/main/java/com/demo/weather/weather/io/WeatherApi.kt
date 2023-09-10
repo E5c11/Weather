55f21b9f2d8f233e6d06c678387254b8218c8058
Emmanuel <emmanuelconradie@gmail.com>
2023-09-10 23:32:31 +0200

    ...

--- app/src/main/java/com/demo/weather/weather/io/WeatherApi.kt
+++ app/src/main/java/com/demo/weather/weather/io/WeatherApi.kt
@@ -1,5 +1,6 @@
 package com.demo.weather.weather.io
 
+import com.demo.weather.BuildConfig
 import com.demo.weather.weather.data.weather.WeatherDto
 import com.demo.weather.weather.data.stations.StationsDto
 import retrofit2.Response
@@ -10,7 +11,7 @@ import retrofit2.http.Query
 interface WeatherApi {
 
     @Headers(
-        "x-rapidapi-key: d6ca539fb1mshc59955312733a25p1c5c8ejsne2dc981ca13c",
+        "x-rapidapi-key: ${BuildConfig.API_KEY}",
         "x-rapidapi-host: meteostat.p.rapidapi.com"
     )
     @GET("stations/nearby")
@@ -20,7 +21,7 @@ interface WeatherApi {
     ): Response<StationsDto>
 
     @Headers(
-        "x-rapidapi-key: d6ca539fb1mshc59955312733a25p1c5c8ejsne2dc981ca13c",
+        "x-rapidapi-key: ${BuildConfig.API_KEY}",
         "x-rapidapi-host: meteostat.p.rapidapi.com"
     )
     @GET("stations/hourly")
